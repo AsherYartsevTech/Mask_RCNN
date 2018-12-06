fe3b310709b3460ead12462b834cb71b469d457b
simon <simon.lousky@campus.technion.ac.il>
2018-12-06 17:01:30 +0200

    the model successfully trains one epoch on our dataset

--- samples/cucumber/cucumber.py
+++ samples/cucumber/cucumber.py
@@ -72,7 +72,7 @@ class CucumberConfig(Config):
     IMAGES_PER_GPU = 1
 
     # Number of classes (including background)
-    NUM_CLASSES = 1 + 80  # background + 3 shapes
+    NUM_CLASSES = 1 + 1  # background + fruit
 
     # Use small images for faster training. Set the limits of the small side
     # the large side, and that determines the image shape.
